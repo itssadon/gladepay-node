8714c10a50d63093c804608e65ebbdce1414c610
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-08-25 23:21:49 +0100

    Added function to verify an account name

--- resources/resource.js
+++ resources/resource.js
@@ -23,5 +23,18 @@ module.exports = {
     params: [{
       "inquire": "banks"
     }]
+  },
+
+  /**
+   * Verify The Account Name
+   */
+  account_name: {
+    method: "get",
+    route: [route].join(""),
+    params: [
+      {"inquire": "accountname"},
+      "accountnumber*",
+      "bankcode*"
+    ]
   }
 };
