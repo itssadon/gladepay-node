d52f611a8257f92f3cd2b062f2af3f8031a26d4a
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-08-29 08:34:28 +0100

    Added Travis CI

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var gladepay = require('../index');
+
+describe('#gladepay', function () {
+    it('should initiate an instance of gladepay', function () {
+        
+    });
+});
