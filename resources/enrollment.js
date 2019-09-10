cf6d7ad2ca95aa2bed818ef52b52f1e4f3d828bd
Abubakar Hassan <abubakarhassan59@gmail.com>
2019-09-10 14:12:24 +0100

    Add function to create a client

--- resources/enrollment.js
+++ resources/enrollment.js
@@ -2,3 +2,20 @@
 
 var route = "/clients";
 
+module.exports = {
+    /**
+     * Create Profile
+     */
+    createProfile: {
+        method: "put",
+        route: route,
+        params: [
+            "data*",
+            "verify_bvn"
+        ],
+        data: {
+            "action": "create",
+            "status": "pending_approval"
+        }
+    }
+};
